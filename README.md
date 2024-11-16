2b06122f88731eaad15ec6483ea29ec50f7de275
tinogithub <public@mugimendu.com>
2024-11-16 11:22:11 +0200

    Script rewrite. Now calculates version

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 A Python script to determine the After Effects Version used to save the project.
 
-I share this little script in the expectation that the community shares more data on the different After Effects builds.
+This script is rewritten and now actually calculates the project version instead of just comparing a HEX string in a data table.
+
+After some more data analysis I finally understood how the project version is encoded in the project file. The analysis will be added later in here.
+
+I have kept the JSON data for further analysis and to look up the year the software version was released. 
 
 ## How to run
 
@@ -13,15 +17,6 @@ I share this little script in the expectation that the community shares more dat
 Under the folder **aftereffects_projects** there are some project files to test the script
 For example: `py afx-version-check.py aftereffects_projects/2023_23-3-0_build53.aep`
 
-## How to contribute
+## Analysis
 
-1. Get a HEX-Editor
-2. Open your After Effects project file in the HEX-Editor and go to the position 20(as Decimals) or 32(as HEX)
-3. The bytes should begin with **00 5E**, **00 5D** or **00 5C**
-4. Copy these bytes including the next 5 bytes. You and up with something like this **00 5E 00 04 0B 39 86**
-5. Take your new information and add a new line in the **ae-builds.json** file according to the other examples
-6. Example: **00 5E 00 04 0B 39 86** will be `{"hex": "005E00040B3986", "version": "2023, v23.3.0"},`
-7. Whereas **23.3.0** is the version given in After Effects under "Help" -> "About After Effects"
-8. Commit your addition or just write and issue containing your new data
-
-Thank you.
+to be done...
--- afx-version-check.py
+++ afx-version-check.py
@@ -1,16 +1,62 @@
-import sys, json, os
-from inspect import getsourcefile
-import ctypes  # An included library with Python install.   
+import sys, binascii
+
+def is_set(x, n):
+    # Check if a bit is set at a given position
+    return x & 1 << n != 0
+
+def hex_to_binary(hex_string):
+    # Create a 64-bit string
+    value = int(hex_string, 16)
+    return bin(value)[2:].zfill(64)
+
+def is_beta(myInt):
+    if not is_set(myInt, 0) and not is_set(myInt, 1):
+        return True
+    return False
+
+def patch(binary_str):
+    patch = binary_str[58:61]
+    patch = int(patch, 2)
+
+    return patch
+
+def minor(binary_str):
+    minor = binary_str[54:57]
+    minor = int(minor, 2)
+
+    return minor
+
+def major(binary_str):
+    major_partB = binary_str[50:53]
+    major_partA = binary_str[43:46]
+    major = int(major_partA + major_partB, 2)
+
+    return major
+
+def systemos(binary_str):
+    if (binary_str[46:50] == '1110'):
+        return 'Mac Arm 64'
+    if (binary_str[46:50] == '1100'):
+        return 'Win'
+    if (binary_str[46:50] == '1101'):
+        return 'Mac'
+
+    return 'unknown'
+
+def hex_to_version(hex_string):
+    binary_string = hex_to_binary(hex_string)
+    myInt = int(hex_string, 16)
+
+    beta = ''
+    if is_beta(myInt):
+        beta = ' BETA'
+
+    return 'v' + str(major(binary_string)) + '.' + str(minor(binary_string)) + "." + str(patch(binary_string)) + beta + ' (' + str(systemos(binary_string)) + ')'
 
 BUFFER_SIZE = 40  
 filename = sys.argv[1]
 assert BUFFER_SIZE % 2 == 0
 START_HEADER = b'\x68\x65\x61\x64'
-
-with open(os.path.dirname(getsourcefile(lambda:0)) + '\\ae-builds.json', 'r') as json_file:
-    json_data = json.load(json_file)
-
-hex_version_map = {bytes.fromhex(item['hex']): item['version'] for item in json_data}
 
 with open(filename, 'rb') as f:
     count = 0
@@ -20,16 +66,17 @@ with open(filename, 'rb') as f:
         if p != -1:
             print("After Effects Version:")
 
-            check = "Unknown"
+            version = "Unknown"
             currBuffer = buffer[32:39]
+            converted = binascii.hexlify(bytearray(currBuffer))
+            hexstring = converted.decode("utf-8")
 
-            if currBuffer in hex_version_map:
-                check = hex_version_map[currBuffer]
+            version = hex_to_version(hexstring)
 
-            # At 40 is the build number stored
+            # At 40 the build number is stored
             currBuild = ord(buffer[39:40])
             
-            print("After Effects",check,"(Build",currBuild,")")
+            print("After Effects",version,"(Build",currBuild,")")
             input("Press Enter to continue...")
             break
         else:
